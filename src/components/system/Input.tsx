8736dc3956f43545803ff39e30327505f1c19f4c
WhySoBad <49595640+WhySoBad@users.noreply.github.com>
2022-01-17 19:23:24 +0100

    init

---
+++ src/components/system/Input.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface InputProps {}
+
+/**
+ * Input component
+ */
+
+const Input: React.FC<InputProps> = (): JSX.Element => {
+  return <></>;
+};
+
+export default Input;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const HomePage: NextPage = (): JSX.Element => {
+  return <main>Main</main>;
+};
+
+export default HomePage;
