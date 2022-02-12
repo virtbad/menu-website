0e05dd0eafa0ecdffe7fc2fd8afa9295c98ca64e
Joshua <joshua.c.stalder@gmail.com>
2022-02-12 14:48:04 +0100

    Comment box almost finished (except requests)

--- src/classes/Comment.class.ts
+++ src/classes/Comment.class.ts
@@ -1,4 +1,9 @@
+import {User} from "./User.class";
+
 export class Comment {
+
+    private _id: string;
+
     private _user: User;
 
     private _title: string;
@@ -9,13 +14,18 @@ export class Comment {
     private _edited: boolean;
 
     // I guess for debugging, later the fields from the api can directly get deserialized into here
-    constructor(user: User, title: string, content: string, rating: number, created: Date, edited: boolean) {
+    constructor(id: string, user: User, title: string, content: string, rating: number, created: Date, edited: boolean) {
+        this._id = id;
         this._user = user;
         this._title = title;
         this._content = content;
         this._rating = rating;
         this._created = created;
         this._edited = edited;
+    }
+
+    public get id(): string {
+        return this._id;
     }
 
     public get user(): User {
